8fbbce6d62305ea335ae3eb0b949de44ee5b0a0a
Michael Gold <goldmichael@gmail.com>
2022-01-31 13:15:21 -0500

    fix: output file name

--- convert_assets.py
+++ convert_assets.py
@@ -7,7 +7,7 @@ source_file_list =  glob.glob("../source/assets/*.glb")
 
 for input_file_name in source_file_list:
     # input_file_name = os.path.split(input_file_name)[1]
-    output_file_name = "../dist/assets/{}.usdz".format(os.path.splitext(input_file_name)[0])
+    output_file_name = "../dist/assets/{}.usdz".format(os.path.splitext(input_file_name)[1])
     print(output_file_name)
     subprocess.call("python run_usd.py usdzconvert/usdzconvert {} {}".format(input_file_name, output_file_name), shell=True)
 
