31174ecb4f121adbfeb1e7c001bfd669522cad51
Michael Gold <goldmichael@gmail.com>
2022-01-31 13:00:56 -0500

    fix: update for python 2

--- convert_assets.py
+++ convert_assets.py
@@ -9,7 +9,7 @@ for input_file_name in source_file_list:
     input_file_name = os.path.split(input_file_name)[1]
     output_file_name = "../dist/assets/{}.usdz".format(os.path.splitext(input_file_name)[0])
     print(output_file_name)
-    subprocess.run("python run_usd.py usdzconvert/usdzconvert {} {}".format(input_file_name, output_file_name), shell=True, check=True )
+    subprocess.call("python run_usd.py usdzconvert/usdzconvert {} {}".format(input_file_name, output_file_name), shell=True, check=True )
 
 for glb_file in source_file_list:
     print(glb_file)
